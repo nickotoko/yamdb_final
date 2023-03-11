0c47899976164832284c150945eee31a3e036b84
Nikolay <nickotoko@yandex.ru>
2023-03-11 16:46:05 +0300

    Settings of project #9

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -66,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "api_yamdb.wsgi.application"
 
-
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
